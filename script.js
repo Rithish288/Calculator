250dcd329ab90b5afb53a4f6dfa6850b852b3402
Rithish <rithish.k@akshayahgs.org>
2021-05-05 20:38:52 +0530

    completed stylings and structure of calculator

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+
+function myFunction() {
+    var key, i;
+    key = document.querySelectorAll(".key");
+    for (i = 0; i < key.length; i++) {
+      key[i].style.fontSize = "150%";
+    }
+  }
+
+  myFunction()
+
 console.log('Hello world!')
